6a35e68bc83c0a8412685a605381681b1f8a90aa
Panni Belevski <pannibel@gmail.com>
2022-10-15 21:40:54 +0200

    ah commitments

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict"
+
+
+
+const mainTitle = document.querySelector("#main_title h1");
+const aboutText = document.querySelector("#about");
+
+window.onscroll = function() { 
+    console.log(document.documentElement.scrollTop);
+    if (document.documentElement.scrollTop > 320 && window.innerWidth > 400) {
+        mainTitle.classList.remove("fixed");
+        mainTitle.style.marginTop = "320px";
+        aboutText.classList.remove("hidden");
+    }
+}
+
+function removeStuff() {
+    if (window.innerWidth > 400) {
+        mainTitle.classList.remove("fixed");
+        mainTitle.style.marginTop = "320px";
+        aboutText.classList.remove("hidden");
+    }
+}
+
+removeStuff()
