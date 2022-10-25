cd97f1b2b16a563defd3c1ba7e963604d88ad38d
Panni Belevski <pannibel@gmail.com>
2022-10-25 19:17:57 +0200

    first version

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const mainTitle = document.querySelector("#main_title h1");
+const mainTitle = document.querySelector("#main_title_h1");
 const aboutText = document.querySelector("#about");
 const assignments = document.querySelector("#assignments");
 const width = document.documentElement.clientWidth;
@@ -8,21 +8,32 @@ const width = document.documentElement.clientWidth;
 window.onscroll = function() {
 
     if (width > 420) {
-            if (document.documentElement.scrollTop > 230) {
-                mainTitle.classList.remove("fixed");
-                mainTitle.style.marginTop = "40vh";
-            }
+            if (document.documentElement.scrollTop > 400) {
+                mainTitle.textContent = "ABOUT ME";
+            };
+            if (document.documentElement.scrollTop > 1000) {
+                mainTitle.textContent = "PROJECTS";
+            };
+            if (document.documentElement.scrollTop > 1800) {
+                mainTitle.textContent = "CONTACT";
+            };
+            if (document.documentElement.scrollTop > 2500) {
+                mainTitle.textContent = "HIRE ME PLS";
+            };
+            if (document.documentElement.scrollTop < 400) {
+                mainTitle.textContent = "PANNIBEL";
+            };
     }
 }
 
 
-const navLinks = document.querySelectorAll("nav ul a");
+/* const navLinks = document.querySelectorAll("nav ul a");
 
 navLinks.forEach(link => link.addEventListener("click", () => {
     mainTitle.classList.remove("fixed");
     mainTitle.style.marginTop = "320px";
     aboutText.classList.remove("hidden");
-}))
+})) */
 
 
 /* const firstLink = document.querySelector("#firstlink");
