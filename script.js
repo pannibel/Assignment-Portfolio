56340545e12c347076e985fdbfa79a49985338e2
Panni Belevski <pannibel@gmail.com>
2022-10-27 13:43:12 +0200

    commitmentssss

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 const mainTitle = document.querySelector("#main_title_h1");
 const aboutText = document.querySelector("#about");
 const assignments = document.querySelector("#assignments");
+const bgd1 = document.querySelector("#bgd_1");
+const bgd2 = document.querySelector("#bgd_2");
 const width = document.documentElement.clientWidth;
 
 window.onscroll = function() {
 
-    if (width > 420) {
+    if (width > 1152) {
             if (document.documentElement.scrollTop > 400) {
                 mainTitle.textContent = "ABOUT ME";
             };
@@ -24,9 +26,27 @@ window.onscroll = function() {
                 mainTitle.textContent = "PANNIBEL";
             };
     }
+
+
+
+    if (width < 1152) {
+        if (document.documentElement.scrollTop > 500) {
+            mainTitle.textContent = "ABOUT ME";
+        };
+        if (document.documentElement.scrollTop > 1100) {
+            mainTitle.textContent = "PROJECTS";
+        };
+        if (document.documentElement.scrollTop > 1700) {
+            mainTitle.textContent = "CONTACT";
+        };
+        if (document.documentElement.scrollTop > 2200) {
+            mainTitle.textContent = "HIRE ME PLS";
+        };
+        if (document.documentElement.scrollTop < 500) {
+            mainTitle.textContent = "PANNIBEL";
+        };
 }
-
-
+}
 /* const navLinks = document.querySelectorAll("nav ul a");
 
 navLinks.forEach(link => link.addEventListener("click", () => {
